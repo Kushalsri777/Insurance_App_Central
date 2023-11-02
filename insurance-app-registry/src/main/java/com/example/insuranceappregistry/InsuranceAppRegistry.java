d72e1890b9537c31564a9ee257579745ced1ef8f
kushal srivastava <kushalsrivastava777@gmail.com>
2023-11-02 09:16:55 -0700

    Added Registry ms

---
+++ insurance-app-registry/src/main/java/com/example/insuranceappregistry/InsuranceAppRegistry.java
@@ -0,0 +1,13 @@
+package com.example.insuranceappregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceAppRegistry {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceAppRegistry.class, args);
+	}
+
+}
