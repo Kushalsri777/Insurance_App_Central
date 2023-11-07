30be18e307a5ff2e40adbad6355bb2e7dc9df9eb
Km Indu <butola.indu02@gmail.com>
2023-11-06 22:25:40 -0800

    updates in cart
    openapi to cart

--- insurance-cart-microservice/src/main/java/com/example/insurancecartmicroservice/InsuranceCartMicroserviceApplication.java
+++ insurance-cart-microservice/src/main/java/com/example/insurancecartmicroservice/InsuranceCartMicroserviceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 @ComponentScan("com")
 @EntityScan("com.example.entity")
 @EnableJpaRepositories("com.example.repo")
+@OpenAPIDefinition(info = @Info(title = "Insurance Cart API", description = "Manage Insurance Cart", version = ""))
 public class InsuranceCartMicroserviceApplication {
 
 	public static void main(String[] args) {
