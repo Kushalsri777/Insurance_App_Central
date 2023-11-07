e678de7f68a02c26458f2a78c6d06f2085e045bc
Km Indu <butola.indu02@gmail.com>
2023-11-06 23:20:18 -0800

    Update InsuranceServiceApplication.java
    insuranceService api

--- InsuranceMicroservice/src/main/java/com/example/insuranceservice/InsuranceServiceApplication.java
+++ InsuranceMicroservice/src/main/java/com/example/insuranceservice/InsuranceServiceApplication.java
@@ -7,10 +7,14 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @ComponentScan("com")
 @EnableJpaRepositories("com.example.repo")
 @EntityScan("com.example.entities")
+@OpenAPIDefinition(info = @Info(title = "Insurance Service API", description = "Manage Insurance Policies", version = ""))
 //@EnableEurekaClient
 public class InsuranceServiceApplication {
 
