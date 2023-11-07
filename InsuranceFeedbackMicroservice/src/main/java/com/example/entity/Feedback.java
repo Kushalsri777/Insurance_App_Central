c3024ead9328a28635ddff6b05c1f453ab74127f
Km Indu <butola.indu02@gmail.com>
2023-11-07 11:07:02 -0800

    Update Feedback.java
    openapi to feedback

--- InsuranceFeedbackMicroservice/src/main/java/com/example/entity/Feedback.java
+++ InsuranceFeedbackMicroservice/src/main/java/com/example/entity/Feedback.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import io.swagger.v3.oas.annotations.media.Schema; 
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,8 +24,12 @@ import lombok.ToString;
 public class Feedback {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Schema(description = "The unique identifier for the feedback")
 	private Long feedbackId;
+	@Schema(description = "The date of the feedback")
 	private Date feedbackDate;
+	@Schema(description = "The ID of the order associated with the feedback")
 	private Long orderId;
+	@Schema(description = "The description of the feedback")
 	private String feedbackDescription;
 }
