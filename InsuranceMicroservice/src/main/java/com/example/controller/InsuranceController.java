4d6b0a8b467776d05ff6873d0028aefa085d981b
Sudipto-Ghosh786 <sudiptoghosh7869@gmail.com>
2023-11-07 06:09:06 -0800

    Added buyPolicy feature in insurance microservice.

--- InsuranceMicroservice/src/main/java/com/example/controller/InsuranceController.java
+++ InsuranceMicroservice/src/main/java/com/example/controller/InsuranceController.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import com.example.constants.UriConstants;
 import com.example.entity.PolicyCatalogue;
+import com.example.model.AllItemsInCartResponse;
+import com.example.model.CreateOrderFromCartRequest;
+import com.example.model.CreateOrderFromCartResponse;
 import com.example.service.PolicyCatalogueService;
 
 @RestController
@@ -14,6 +19,27 @@ public class InsuranceController {
 
     @Autowired
     private PolicyCatalogueService service;
+    @Autowired
+    private RestTemplate restTemplate;
+    
+    @PostMapping("/buypolicy/{userId}")
+    public ResponseEntity<Object> buyPolicy(@PathVariable final int userId){
+    	AllItemsInCartResponse cartItemsResponseBody = restTemplate.getForEntity(String.format(
+    			String.format(UriConstants.GET_ALL_ITEMS_FROM_CART_URL, userId), userId), 
+    			AllItemsInCartResponse.class).getBody();
+    	restTemplate.postForEntity(UriConstants.CREATE_ORDER_FROM_CART_URL, CreateOrderFromCartRequest.builder()
+    			.userId(userId).isPaymentDone(Boolean.TRUE).build(), CreateOrderFromCartResponse.class);
+        HttpHeaders header = new HttpHeaders();
+        return ResponseEntity.ok().headers(header).body("DONE !!");
+    }
+
+    @GetMapping("/getallpolicies")
+    public ResponseEntity<Object> getAllPolicies(){
+        HttpHeaders header = new HttpHeaders();
+        return ResponseEntity.ok()
+        .headers(header)
+        .body(service.getAllPolicies());
+    }
 
     @PostMapping("addpolicy")
     public ResponseEntity<Object> addPolicy(@RequestBody PolicyCatalogue policy) {
@@ -31,14 +57,6 @@ public class InsuranceController {
                 .body("Deleted")
                 : ResponseEntity.ok()
                         .body("User Not Found");
-    }
-
-    @GetMapping("/getallpolicies")
-    public ResponseEntity<Object> getAllPolicies() {
-        HttpHeaders header = new HttpHeaders();
-        return ResponseEntity.ok()
-                .headers(header)
-                .body(service.getAllPolicies());
     }
 
     @GetMapping("/getpolicybyid/{id}")
