94ac4d9f0c42400a542b9a863fff4c722e5245ab
Km Indu <butola.indu02@gmail.com>
2023-11-07 10:26:07 -0800

    Update Discount.java
    openapi entity

--- discount/src/main/java/com/entity/Discount.java
+++ discount/src/main/java/com/entity/Discount.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import io.swagger.v3.oas.annotations.media.Schema; // Import OpenAPI Schema
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,10 +19,14 @@ import lombok.ToString;
 @ToString
 @Builder
 @Entity
+@Schema(description = "Discount entity") // OpenAPI Schema
 public class Discount {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Schema(description = "The ID of the discount")
 	private Long discountId;
+	@Schema(description = "The ID of the associated policy")
 	private Long policyId;
+	@Schema(description = "The value of the discount")
 	private Double value;
 }
